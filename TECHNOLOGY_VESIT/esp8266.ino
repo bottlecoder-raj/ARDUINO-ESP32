2a381fc706aec9056f89b6a68bcebdb252683394
Raj Narayan <hazrarajnarayan55@gmail.com>
2026-03-20 11:13:58 +0530

    Update esp8266.ino

--- TECHNOLOGY_VESIT/esp8266.ino
+++ TECHNOLOGY_VESIT/esp8266.ino
@@ -1,41 +1,74 @@
 #include <ESP8266WiFi.h>
 
-const char* ssid = "YOUR_WIFI_NAME";
-const char* password = "YOUR_WIFI_PASSWORD";
+const char* ssid = "HELLO";
+const char* password = "12345678";
 
 WiFiServer server(80);
 
+// LED Pins
+#define LED_POWER 2    // GPIO2 (D4)
+#define LED_STATUS 16  // GPIO16 (D0)
+
+// Blink control
+unsigned long lastBlink = 0;
+bool ledState = LOW;
+
 void setup() {
-  Serial.begin(9600);   // Must match Arduino espSerial
+  Serial.begin(9600);
+
+  pinMode(LED_POWER, OUTPUT);
+  pinMode(LED_STATUS, OUTPUT);
+
+  digitalWrite(LED_POWER, LOW);   // Power ON LED
 
   WiFi.begin(ssid, password);
 
-  // Wait for connection
+  Serial.print("Connecting to WiFi");
+
+  // 🔥 WAIT until connected
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
+    Serial.print(".");
+    
+    // Slow blink while connecting
+    digitalWrite(LED_STATUS, LOW);
+    delay(100);
+    digitalWrite(LED_STATUS, HIGH);
   }
 
-  server.begin();
-
-  // Print IP (open Serial Monitor ON ESP to see this)
+  Serial.println("\nConnected!");
+  Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
+
+  server.begin();
+  Serial.write('S');
 }
 
 void loop() {
+
+  // ✅ Connected → Blink LED continuously
+  unsigned long currentMillis = millis();
+  if (currentMillis - lastBlink >= 1000) {
+    lastBlink = currentMillis;
+    ledState = !ledState;
+    digitalWrite(LED_STATUS, ledState);
+  }
+
+  // 🌐 Handle client
   WiFiClient client = server.available();
   if (!client) return;
 
   String request = client.readStringUntil('\r');
   client.flush();
 
-  // 🔥 Send commands to Arduino
+  // Send commands to Arduino
   if (request.indexOf("/F") != -1) Serial.write('F');
   if (request.indexOf("/B") != -1) Serial.write('B');
   if (request.indexOf("/L") != -1) Serial.write('L');
   if (request.indexOf("/R") != -1) Serial.write('R');
   if (request.indexOf("/S") != -1) Serial.write('S');
 
-  // Simple Web Page
+  // Web Page
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
   client.println("");
@@ -48,4 +81,5 @@ void loop() {
   client.println("<a href=\"/S\">Stop</a><br>");
 
   delay(1);
+  client.stop();
 }
